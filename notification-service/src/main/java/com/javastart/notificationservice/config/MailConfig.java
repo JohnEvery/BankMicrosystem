167d47db7fe924a1d96ab0b965ee354250c4636a
Ivan Shchipalkin <glumven@gmail.com>
2020-07-31 18:39:29 +0300

    Hide mailSender email and pass

--- notification-service/src/main/java/com/javastart/notificationservice/config/MailConfig.java
+++ notification-service/src/main/java/com/javastart/notificationservice/config/MailConfig.java
@@ -16,8 +16,8 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("testjavastart@gmail.com");
-        mailSender.setPassword("javastart0320");
+        mailSender.setUsername("emailserver@gmail.com");
+        mailSender.setPassword("somepass");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
